e286fbaac6a7dbcd3060f95626e9c4873cec41cd
文詩涵 <ruby422988@gmail.com>
2025-11-25 04:03:50 +0800

    Add files via upload

---
+++ DICD_code_v9/include/data_type.svh
@@ -0,0 +1,17 @@
+// I/O types
+typedef logic signed [15:0] r_t;
+typedef logic signed [7:0]  rho_t;
+typedef logic signed [20:0] eps_t;
+typedef logic [7:0]         theta_t;
+
+// Port types
+typedef logic signed [15:0] phi_t;
+typedef logic signed [15:0] gamma_t;
+typedef logic signed [15:0] mag_t;
+typedef logic signed [12:0] ang_t;
+typedef logic signed [15:0] lambda_t;
+
+// Internal types
+typedef logic signed [11:0] ambm_t;
+typedef logic [9:0]         PI_t;
+typedef logic [7:0]         ratio_t;
