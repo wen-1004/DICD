770cfd537fdf001c6a6e1bc637fa2c2b0245f8e4
文詩涵 <ruby422988@gmail.com>
2025-11-25 03:53:19 +0800

    Add files via upload

---
+++ DICD_code_v9/run_sim.sh
@@ -0,0 +1,22 @@
+g++ -std=c++17 -DUSE_RTL_MINUS \
+  -I"$SYSTEMC_HOME/include" \
+  -Iinclude \
+  -Isrc \
+  -Iobj_minus \
+  -I"$VERILATOR_ROOT/include" \
+  -I"$VERILATOR_ROOT/include/vltstd" \
+  src/EstimatorTop.cpp \
+  src/delay_n.cpp \
+  src/phi_sum.cpp \
+  src/gamma_sum.cpp \
+  src/mag.cpp \
+  src/angle.cpp \
+  src/argmax.cpp \
+  src/select_eps.cpp \
+  sim/main.cpp \
+  verilator/obj_minus/Vminus__ALL.a \
+  "$VERILATOR_ROOT/include/verilated.cpp" \
+  "$VERILATOR_ROOT/include/verilated_threads.cpp" \
+  -L"$SYSTEMC_HOME/lib64" \
+  -lsystemc -lm -pthread \
+  -o full_test
